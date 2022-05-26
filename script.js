86a6443420c0eef95c011d65216a970c76c12cbd
Lucas Israel <lisraelalves92@gmail.com>
2022-05-25 21:16:40 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-window.onload = function () {
-  const bgColor1 = document.querySelectorAll('.color')[0];
-  bgColor1.style.backgroundColor = bgColor1.id;
-  const bgColor2 = document.querySelectorAll('.color')[1];
-  bgColor2.style.backgroundColor = bgColor2.id;
-  const bgColor3 = document.querySelectorAll('.color')[2];
-  bgColor3.style.backgroundColor = bgColor3.id;
-  const bgColor4 = document.querySelectorAll('.color')[3];
-  bgColor4.style.backgroundColor = bgColor4.id;
-};
+const bgColor1 = document.querySelectorAll('.color')[0];
+bgColor1.style.backgroundColor = bgColor1.id;
+const bgColor2 = document.querySelectorAll('.color')[1];
+bgColor2.style.backgroundColor = bgColor2.id;
+const bgColor3 = document.querySelectorAll('.color')[2];
+bgColor3.style.backgroundColor = bgColor3.id;
+const bgColor4 = document.querySelectorAll('.color')[3];
+bgColor4.style.backgroundColor = bgColor4.id;
+
 const ulistC = document.createElement('ul');
 ulistC.id = 'linha';
 document.getElementById('pixel-board').appendChild(ulistC);
@@ -26,3 +25,11 @@ for (let i = 0; i < 5; i += 1) {
     document.getElementById(i).appendChild(divs);
   }
 }
+const corPal = document.getElementById('color-palette');
+corPal.addEventListener('click', (event) => {
+  const selected = document.querySelector('.selected');
+  if (event.target.className === 'color') {
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+});
